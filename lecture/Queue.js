ca396a777f542d6f05acdae8c7899ae6f0393209
강인웅 <woong@gang-in-ungs-MacBook-Air.local>
2022-02-01 23:56:26 +0900

    2.1

---
+++ lecture/Queue.js
@@ -0,0 +1,38 @@
+//array의 shift를 이용해도 que를 이용할 수 없다. 
+
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+  dequeue() {
+    const value = this.queue[this.front];
+    delete this.queue[this.front];
+    this.front += 1;
+    return value;
+  }
+
+  peek() {
+    return this.queue[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(4);
+console.log(queue.dequeue());
+queue.enqueue(8);
+console.log(queue.size());
+console.log(queue.peek());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
