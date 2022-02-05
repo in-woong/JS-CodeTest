276a578d4003c2214cbad640bbbf75a2e6976fb3
강인웅 <woong@gang-in-ungs-MacBook-Air.local>
2022-02-05 16:25:48 +0900

    ADD / 2.5

---
+++ lecture/heap.js
@@ -0,0 +1,68 @@
+class MaxHeap {
+  constructor() {
+    this.heap = [null];
+  }
+  push(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (parentIndex !== 0 && this.heap[parentIndex] < value) {
+      const temp = this.heap[parentIndex];
+      this.heap[parentIndex] = value;
+      this.heap[currentIndex] = temp;
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
+    }
+  }
+
+  pop() {
+    if (this.heap.length === 2) return this.heap.pop();
+    
+    const returnValue = this.heap[1];
+    //마지막 정점이 루트에 위치한다
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+
+    while (
+      this.heap[currentIndex] < this.heap[leftIndex] ||
+      this.heap[currentIndex] < this.heap[rightIndex]
+    ) {
+      if (this.heap[leftIndex] < this.heap[rightIndex]) {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[rightIndex];
+        this.heap[rightIndex] = temp;
+        currentIndex = rightIndex;
+      } else {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[leftIndex];
+        this.heap[leftIndex] = temp;
+        currentIndex = leftIndex;
+      }
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
+    }
+    return returnValue;
+  }
+}
+
+const heap = new MaxHeap();
+heap.push(45);
+heap.push(36);
+heap.push(54);
+heap.push(27);
+heap.push(63);
+console.log(heap.heap);
+//Result is [null, 64,54,45,27,36]
+
+const array = [];
+array.push(heap.pop());
+array.push(heap.pop());
+array.push(heap.pop());
+array.push(heap.pop());
+array.push(heap.pop());
+console.log(array);
