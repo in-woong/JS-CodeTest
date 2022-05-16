0f9454f3e4e26e3fbce81ae673f678fec6da02af
woong <inwoong100@gmail.com>
2022-05-16 23:19:35 +0900

    add /5.11_codeTest_answer

--- zerobase/pb5.11/answer/SmallestSameIndex.js
+++ zerobase/pb5.11/answer/SmallestSameIndex.js
@@ -1,8 +1,4 @@
-/**
- * @param nums {number[]}
- * @param n {number}
- * @return {number}
- */
+
 function solution(nums, n) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === n) return i
