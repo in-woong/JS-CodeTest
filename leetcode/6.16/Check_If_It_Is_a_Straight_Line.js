97e671dbd660c53f33bb63348eb679e68ded1ac1
woong <inwoong100@gmail.com>
2022-06-17 00:08:59 +0900

    add 6.16/leetcode_easySecond

--- leetcode/6.16/Check_If_It_Is_a_Straight_Line.js
+++ leetcode/6.16/Check_If_It_Is_a_Straight_Line.js
@@ -1,6 +1,30 @@
 // You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
 
-function solution(coordinates) {}
+function solution(coordinates) {
+  let incline;
+  for (let i = 1; i < coordinates.length; i++) {
+    if (i == 1) {
+      const [x1, y1] = coordinates[0];
+      const [x2, y2] = coordinates[1];
+      if (x2 - x1 == 0 || y2 - y1 == 0) incline = 0;
+      else {
+        incline = (x2 - x1) / (y2 - y1);
+      }
+    } else {
+      const [x1, y1] = coordinates[i - 1];
+      const [x2, y2] = coordinates[i];
+      let newIncline;
+      if (x2 - x1 == 0 || y2 - y1 == 0) {
+        newIncline = 0;
+      } else {
+        newIncline = (x2 - x1) / (y2 - y1);
+      }
+      console.log(incline, newIncline);
+      if (incline !== newIncline) return false;
+    }
+  }
+  return true;
+}
 
 const input = [
   [
@@ -18,6 +42,14 @@ const input = [
     [4, 5],
     [5, 6],
     [7, 7],
+  ],
+
+  [
+    [-3, -2],
+    [-1, -2],
+    [2, -2],
+    [-2, -2],
+    [0, -2],
   ],
 ];
 
