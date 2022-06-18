8752a4d12b7dea566908a8a93a9124f4cc469eda
woong <inwoong100@gmail.com>
2022-06-18 09:54:49 +0900

    add 6.17/leetcode_InvertBinaryTree_easy

---
+++ leetcode/6.17/InvertBinaryTree.js
@@ -0,0 +1,31 @@
+function solution(root) {
+    invert(root);
+    return root
+    
+};
+
+function invert(node){
+    if(!node)return;
+    if(node.left!==null&&node.right!==null){
+        const temp = node.left;
+        node.left =node.right;
+        node.right = temp;
+    }else if (node.left==null && node.right!==null){
+        const temp = null;
+        node.left = node.right;
+        node.right = null
+    } else if(node.right==null && node.left!==null){
+        const temp = null;
+        node.right = node.left;
+        node.left = null
+    }
+    invert(node.left);
+    invert(node.right);
+    
+}
+
+const input = [];
+
+for (let i = 0; i < input.length; i++) {
+  console.log(`${i + 1}: ${solution(input[i])}`);
+}
