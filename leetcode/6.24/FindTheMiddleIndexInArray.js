63cb189e47d60be386fc73dbf8e03b1070085ceb
woong <inwoong100@gmail.com>
2022-06-25 10:30:31 +0900

    add 6.24/leetcode_easy_2

---
+++ leetcode/6.24/FindTheMiddleIndexInArray.js
@@ -0,0 +1,27 @@
+function solution(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    
+    if (sum(0, i, nums) === sum(i + 1, nums.length, nums)) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+function sum(start, end, nums) {
+  let sum = 0;
+  for (let i = start; i < end; i++) {
+    sum += nums[i];
+  }
+  return sum;
+}
+
+const input = [
+  [2, 3, -1, 8, 4],
+  [1, -1, 4],
+  [2, 5],
+];
+
+for (let i = 0; i < input.length; i++) {
+  console.log(`${i + 1}: ${solution(input[i])}`);
+}
