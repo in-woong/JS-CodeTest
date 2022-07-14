5052b6ddbc1fd670a409312181b85f11f1603b62
woong <inwoong100@gmail.com>
2022-07-14 19:42:07 +0900

    add 7.14/leetcode_easy_2

---
+++ leetcode/July/7.14/GuessNumberHigherOrLower.js
@@ -0,0 +1,24 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+  let left = 1;
+  let right = n;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let answer = guess(mid);
+    if (answer > 0) left = mid + 1;
+    if (answer < 0) right = mid - 1;
+    if (answer == 0) return mid;
+  }
+};
