99a7c0ee89515e37adb558a676945d9fe8a0900f
woong <inwoong100@gmail.com>
2022-07-22 23:48:08 +0900

    add 7.22/leetcode_easy

---
+++ leetcode/July/7.22/Easy_FindAllNumbersDisappearedInAnArray.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+  const answer = [];
+  const n = nums.length;
+  for (let i = 1; i <= n; i++) {
+    if (!nums.includes(i)) answer.push(i);
+  }
+  return answer;
+};
