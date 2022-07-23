6927a015ceb18cc4a9d1f2f3f459098620beabee
woong <inwoong100@gmail.com>
2022-07-23 16:03:15 +0900

    add 7.23/leetcode_easy_2

---
+++ leetcode/July/7.23/Easy_CheckIfAllCharactersHaveEqualNumberOfOccurrence.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areOccurrencesEqual = function (s) {
+  const sArray = s.split('');
+
+  const map = new Map();
+  for (let i = 0; i < sArray.length; i++) {
+    if (!map.has(sArray[i])) {
+      map.set(sArray[i], 1);
+    } else {
+      map.set(sArray[i], map.get(sArray[i]) + 1);
+    }
+  }
+
+  const set = new Set(Array.from(map.values()));
+  let answer = set.size == 1 ? true : false;
+
+  return answer;
+};
